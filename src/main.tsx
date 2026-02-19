56a48c67300bcf8d8aa98a32a62b738a112a93d1
shinitio324 <shinitio324@gmail.com>
2026-02-19 14:47:02 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+// @ts-ignore
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
